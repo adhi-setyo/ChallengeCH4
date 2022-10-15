ad268a47d4f974264166c83bd3ca776a92645378
adhi setyo <AyodyaCitra1>
2022-10-15 18:01:04 +0700

    add custom dialog again

---
+++ app/src/main/java/com/adhi/challengech4/ui/dialog/ResultDialogFragment.kt
@@ -0,0 +1,44 @@
+package com.adhi.challengech4.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.adhi.challengech4.R
+import com.adhi.challengech4.databinding.FragmentResultDialogBinding
+
+class ResultDialogFragment : DialogFragment() {
+
+    private lateinit var binding: FragmentResultDialogBinding
+    private var listener : OnMenuSelectedListener? = null
+
+    fun setOnMenuSelectedListener(listener: OnMenuSelectedListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentResultDialogBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvPlayAgain.setOnClickListener{
+            listener?.onPlayAgain(this)
+        }
+        binding.tvBackMenu.setOnClickListener{
+            listener?.onBacToMenu(this)
+        }
+    }
+}
+
+interface OnMenuSelectedListener {
+    fun onPlayAgain(dialog : DialogFragment)
+    fun onBacToMenu(dialog : DialogFragment)
+
+}
